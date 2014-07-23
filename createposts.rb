8db6787cbf29d6ae4bc969e6360a20d03f2ed8af
marketfresh <matt@vestor.co>
2014-07-23 11:12:13 -0400

    createposts code

---
+++ createposts.rb
@@ -0,0 +1,19 @@
+class CreatePosts < ActiveRecord::Migration
+	def self.up
+		create_table :posts do |t|
+			t.string :name
+			t.text :address
+			t.date :dob
+			t.text :desire
+			t.text :interests
+			t.text :hobby
+			t.text :signature
+
+			t.timestamps
+		end
+	end
+
+	def self.down
+		drop_table :posts
+	end
+end
